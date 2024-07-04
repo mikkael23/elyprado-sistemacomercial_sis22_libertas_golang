d122354df75e194a36f968bed12280294c8b0499
Gabriel Paulino Alves <128816401+Gabriel-Paulino-Alves@users.noreply.github.com>
2024-07-04 18:32:29 -0300

    Update produto.js

--- go-crud-api/static/produto.js
+++ go-crud-api/static/produto.js
@@ -61,7 +61,7 @@ function listar() {
     const txtpesquisa = document.getElementById("txtpesquisa");
 
 
-    fetch("http://127.0.0.1:8080/static/produto.html?pesquisa=" + txtpesquisa.value)
+    fetch("http://127.0.0.1:8080/produto?pesquisa=" + txtpesquisa.value)
     .then(resp => resp.json())
     .then(dados => mostrar(dados));
 }
