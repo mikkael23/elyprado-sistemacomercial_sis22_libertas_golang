5f344f024f526cf4866aa682d8f13acaef15cbdf
joao12pedro <114536833+joao12pedro@users.noreply.github.com>
2024-07-06 15:39:15 -0300

    Update contasapagar.go

--- go-crud-api/models/contasapagar.go
+++ go-crud-api/models/contasapagar.go
@@ -2,10 +2,10 @@ package models
 
 type Conta struct {
 	ID_Pagar     int    `json:"idpagar"`
-	data         string `json:"data"`
-	valor        string `json:"valor"`
-	vencimento   string `json:"vencimento"`
-	pagamento    string `json:"pagamento"`
-	valorpago    string `json:"valorpago"`
-	idfornecedor string `json:"idfornecedor"`
+	Data         string `json:"data"`
+	Valor        string `json:"valor"`
+	Vencimento   string `json:"vencimento"`
+	Pagamento    string `json:"pagamento"`
+	Valorpago    string `json:"valorpago"`
+	Idfornecedor string `json:"idfornecedor"`
 }
